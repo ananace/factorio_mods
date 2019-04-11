d09ee265bed1d6f234154c528bfaaec5da5248d9
Alexander Olofsson <ace@haxalot.com>
2019-04-12 01:10:27 +0200

    Implement the basics of modpack handling

--- lib/factorio_mods/cli/pack.rb
+++ lib/factorio_mods/cli/pack.rb
@@ -10,25 +10,45 @@ class FactorioMods::CLI
 
     desc 'show', 'Lists all modpacks'
     def show
-      raise NotImplementedError
+      puts "Available mod packs:"
+      (_cli._cache[:packs] ||= {}).each do |k,_v|
+        puts "- #{k}"
+      end
     end
 
     desc 'new PACK', 'Creates a new modpack'
     def new(pack)
       mods = _cli._mods.enabled_mods
-      pack = mods.map(&:name)
+      mods = mods.map(&:name)
 
-      (_cli._cache[:packs] ||= {})[pack] = pack
+      (_cli._cache[:packs] ||= {})[pack.to_s.to_sym] = mods
+
+      puts "Created #{pack}"
     end
 
     desc 'delete PACK', 'Deletes a new modpack'
-    def delete(_pack)
-      raise NotImplementedError
+    def delete(pack)
+      (_cli._cache[:packs] ||= {}).delete pack.to_s.to_sym
+
+      puts "Removed #{pack}"
     end
 
     desc 'use PACK', 'Switches to a specific modpack'
-    def use(_pack)
-      raise NotImplementedError
+    def use(name)
+      pack = (_cli._cache[:packs] ||= {})[name.to_s.to_sym]
+
+      if pack.nil?
+        puts "Unable to find pack #{name}"
+        return
+      end
+
+      puts "Enabling pack #{name}..."
+
+      _cli._mods.mods.each { |m| m.enabled = false }
+      pack.each do |m|
+        _cli._mods.enable_mod(m)
+      end
+      _cli._mods.save!
     end
 
     no_commands do
